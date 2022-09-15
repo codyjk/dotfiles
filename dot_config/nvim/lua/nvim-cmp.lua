0069ec7236ddbbd9ced64f231253be71ea3e04b0
Cody Joseph Krainock <me@codyjoseph.com>
2022-09-15 14:08:34 -0700

    lsp improvements

--- dot_config/nvim/lua/nvim-cmp.lua
+++ dot_config/nvim/lua/nvim-cmp.lua
@@ -12,8 +12,22 @@ cmp.setup({
 			end,
 		},
 		window = {
-			-- completion = cmp.config.window.bordered(),
-			-- documentation = cmp.config.window.bordered(),
+			completion = {
+				winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+				col_offset = -3,
+				side_padding = 0,
+			},
+		},
+		formatting = {
+			fields = { "kind", "abbr", "menu" },
+			format = function(entry, vim_item)
+				local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+				local strings = vim.split(kind.kind, "%s", { trimempty = true })
+				kind.kind = " " .. strings[1] .. " "
+				kind.menu = "    (" .. strings[2] .. ")"
+
+				return kind
+			end,
 		},
 		mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
