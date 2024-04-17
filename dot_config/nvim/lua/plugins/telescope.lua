fe5a8afad293223251bd37ffc40e4749769de0a9
Cody Joseph Krainock <cody@loop.com>
2024-04-16 22:20:01 -0700

    fix keybindings

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -24,11 +24,13 @@ return {
 			telescope.load_extension("ui-select")
 			telescope.load_extension("gh")
 			-- Find files using Telescope command-line sugar.
+      nmap("<C-p>", builtins.git_files)
 			nmap("<leader>ff", builtins.find_files, "[F]ind [F]iles")
 			nmap("<leader>?", builtins.oldfiles, "[?] Find recently opened files")
 			nmap("<leader>fr", builtins.resume, "[F]ind [R]esume")
 			nmap("<leader>fg", builtins.live_grep, "[F]ind [G]rep")
 			nmap("<leader>fb", builtins.buffers, "[F]ind [B]uffers")
+			nmap("<leader>b", builtins.buffers, "[B]uffers")
 			nmap("<leader>fq", builtins.quickfix, "[F]ind [Q]uickfix")
 			nmap("<leader>fs", function()
 				-- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -38,8 +40,8 @@ return {
 			end, "[F]ind [S]ymbols")
 			nmap("<leader>h", builtins.help_tags, "[F]ind [H]elp")
 			nmap("<leader>f", builtins.builtin, "[F]ind something")
-			nmap("gd", builtins.lsp_definitions, "[G]oto [D]efinition")
-			nmap("gr", builtins.lsp_references, "[G]oto [R]eferences")
+      nmap("gd", builtins.lsp_definitions, "[G]oto [D]efinition")
+      nmap("gr", builtins.lsp_references, "[G]oto [R]eferences")
 			nmap("<leader>fG", builtins.grep_string, "[F]ind [G]rep string")
       nmap("<leader>fr", builtins.resume, "[F]ind [R]esume")
 		end,
