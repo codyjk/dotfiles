c5c61a40a542026a0bc76cfcf1c0924413a790df
Cody Joseph Krainock <cody@loop.com>
2024-04-16 22:12:09 -0700

    Fix gitsigns

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,15 +1,17 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    opts = function(_, opts)
-      opts.current_line_blame = true;
-      opts.current_line_blame_opts.virt_text = true;
-      -- TODO fix
-      -- 'eol' | 'overlay' | 'right_align'
-      -- opts.current_line_blame_opts.virt_text_pos = 'eol';
-      -- opts.current_line_blame_opts.delay = 1000;
-      -- opts.current_line_blame_opts.ignore_whitespace = false;
-      -- opts.current_line_blame_formatter = '\t\t<author>, <author_time:%Y-%m-%d> - <summary> ';
+    config = function ()
+      require('gitsigns').setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = '\t\t<author>, <author_time:%Y-%m-%d> - <summary> '
+      })
     end,
   },
   {
