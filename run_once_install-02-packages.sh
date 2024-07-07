dd89c66db92904e4b2a2d3ef6c18a5d5c5cc1f42
Cody Joseph Krainock <me@codyjoseph.com>
2024-07-07 15:27:18 -0700

    make install script consistent with zsh dependencies

--- run_once_install-02-packages.sh
+++ run_once_install-02-packages.sh
@@ -3,8 +3,8 @@
 # neovim
 ~/.brew/bin/brew install neovim
 
-# starship prompt
-~/.brew/bin/brew install starship
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # nvm && node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -13,12 +13,21 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . $HOME/.nvm/nvm.sh
 nvm install node
 
+# bun
+curl -fsSL https://bun.sh/install | bash
+
+# pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
 # fzf (ignoring user prompts)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --no-bash --no-zsh --no-fish
 
-# asdf
-~/.brew/bin/brew install asdf
-
 # ripgrep
 brew install rg
+
+# powerlevel10k prompt
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# tmux
+~/.brew/bin/brew install tmux
