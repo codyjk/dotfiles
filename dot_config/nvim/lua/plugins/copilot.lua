29362eeea18246d2650508897b0c305d8522d485
Cody Joseph Krainock <me@codyjoseph.com>
2024-07-07 15:27:18 -0700

    make install script consistent with zsh dependencies

--- dot_config/nvim/lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
- "github/copilot.vim",
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', {expr=true, silent=true})
-    vim.g.copilot_node_command = "/Users/cjk/.asdf/installs/nodejs/20.12.1/bin/node"
-  end,
-}
--- run_once_install-04-tmux.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-~/.brew/bin/brew install tmux
