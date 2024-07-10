cec957928455a809ca4a5506e62bba3ea2f30372
Cody Joseph Krainock <me@codyjoseph.com>
2024-07-10 09:07:01 -0700

    update supermaven keys

--- dot_config/nvim/lua/plugins/supermaven.lua
+++ dot_config/nvim/lua/plugins/supermaven.lua
@@ -1,6 +1,14 @@
 return {
   "supermaven-inc/supermaven-nvim",
   config = function()
-    require("supermaven-nvim").setup({})
+  -- Unmap <C-J> (so it can be used as completion
+  vim.api.nvim_set_keymap('i', '<C-J>', '', { noremap = true, silent = true })
+    require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-J>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<Tab>",
+        },
+    })
   end,
 }
