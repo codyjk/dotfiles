d7baa462798e23a4a5e49e13bc4ad938750e014b
Cody Joseph Krainock <me@codyjoseph.com>
2024-09-07 18:00:17 -0700

    Ignore `run_once` if chezmoi is already set up

--- run_once_install-00-xcode.sh
+++ run_once_install-00-xcode.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+if [ -d "$HOME/.local/share/chezmoi" ]; then
+    # Chezmoi is already installed, so we can skip this step
+    exit 0
+fi
+
 xcode-select --install || true
--- run_once_install-01-homebrew.sh
+++ run_once_install-01-homebrew.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+if [ -d "$HOME/.local/share/chezmoi" ]; then
+    # Chezmoi is already installed, so we can skip this step
+    exit 0
+fi
+
 git clone --depth=1 https://github.com/Homebrew/brew ~/.brew
