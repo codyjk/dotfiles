bf07e1fa509a0ffb585d80f34907289c0f595bbf
Cody Joseph Krainock <me@codyjoseph.com>
2024-10-04 16:50:50 -0700

    Remove unnecessary args from lsp-on-attach

--- dot_config/nvim/lua/helpers/lsp-on-attach.lua
+++ dot_config/nvim/lua/helpers/lsp-on-attach.lua
@@ -1,12 +1,24 @@
 local lsp_map = require("helpers.keys").lsp_map
 local on_attach = function(_, bufnr)
 	local builtins = require("telescope.builtin")
-	lsp_map("<leader>e", vim.diagnostic.open_float, bufnr, "Show diagnostics")
-	lsp_map("<leader>rn", vim.lsp.buf.rename, bufnr, "Rename symbol")
-	lsp_map("<leader>ca", vim.lsp.buf.code_action, bufnr, "Code action")
-	lsp_map("gd", builtins.lsp_definitions, bufnr, "Goto Definition")
-	lsp_map("gr", builtins.lsp_references, bufnr, "Goto References")
-	lsp_map("K", vim.lsp.buf.hover, bufnr, "Hover Documentation")
+
+	-- Show diagnostics
+	lsp_map("<leader>e", vim.diagnostic.open_float, bufnr)
+
+  -- Rename symbol
+	lsp_map("<leader>rn", vim.lsp.buf.rename, bufnr)
+
+  -- Code actions
+	lsp_map("<leader>ca", vim.lsp.buf.code_action, bufnr)
+
+  -- Goto definition
+	lsp_map("gd", builtins.lsp_definitions, bufnr)
+
+  -- Goto references
+	lsp_map("gr", builtins.lsp_references, bufnr)
+
+  -- Show hover documentation
+	lsp_map("K", vim.lsp.buf.hover, bufnr)
 end
 
 return on_attach
