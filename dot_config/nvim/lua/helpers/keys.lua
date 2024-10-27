0ce61e1df144a2ded86d253c91b1214c1afec86b
Cody Joseph Krainock <me@codyjoseph.com>
2024-10-27 16:04:58 -0700

    Cleanup

--- dot_config/nvim/lua/helpers/keys.lua
+++ dot_config/nvim/lua/helpers/keys.lua
@@ -1,28 +1,28 @@
 local map = function(mode, lhs, rhs)
-	vim.keymap.set(mode, lhs, rhs, { silent = true })
+  vim.keymap.set(mode, lhs, rhs, { silent = true })
 end
 
 local lsp_map = function(lhs, rhs, bufnr)
-	vim.keymap.set("n", lhs, rhs, { silent = true, buffer = bufnr })
+  vim.keymap.set("n", lhs, rhs, { silent = true, buffer = bufnr })
 end
 
 local set_leader = function(key)
-	vim.g.mapleader = key
-	vim.g.maplocalleader = key
-	map({ "n", "v" }, key, "<nop>")
+  vim.g.mapleader = key
+  vim.g.maplocalleader = key
+  map({ "n", "v" }, key, "<nop>")
 end
 
 return {
-	map = map,
-	lsp_map = lsp_map,
-	set_leader = set_leader,
-	nmap = function(lhs, rhs)
-		map("n", lhs, rhs)
-	end,
-	vmap = function(lhs, rhs)
-		map("v", lhs, rhs)
-	end,
-	imap = function(lhs, rhs)
-		map("i", lhs, rhs)
-	end,
+  map = map,
+  lsp_map = lsp_map,
+  set_leader = set_leader,
+  nmap = function(lhs, rhs)
+    map("n", lhs, rhs)
+  end,
+  vmap = function(lhs, rhs)
+    map("v", lhs, rhs)
+  end,
+  imap = function(lhs, rhs)
+    map("i", lhs, rhs)
+  end,
 }
