7de7dfa8b190a5bdc32992d8a1f3a2154e2df602
Cody Joseph Krainock <me@codyjoseph.com>
2024-10-27 16:04:58 -0700

    Cleanup

--- dot_config/nvim/lua/core/lazy.lua
+++ dot_config/nvim/lua/core/lazy.lua
@@ -1,20 +1,20 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
 local ok, lazy = pcall(require, "lazy")
 if not ok then
-	return
+  return
 end
 
 -- We have to set the leader key here for lazy.nvim to work
--- dot_config/nvim/lua/helpers/lsp-on-attach.lua
+++ dot_config/nvim/lua/helpers/lsp-on-attach.lua
@@ -1,24 +1,24 @@
 local lsp_map = require("helpers.keys").lsp_map
 local on_attach = function(_, bufnr)
-	local builtins = require("telescope.builtin")
+  local builtins = require("telescope.builtin")
 
-	-- Show diagnostics
-	lsp_map("<leader>e", vim.diagnostic.open_float, bufnr)
+  -- Show diagnostics
+  lsp_map("<leader>e", vim.diagnostic.open_float, bufnr)
 
   -- Rename symbol
-	lsp_map("<leader>rn", vim.lsp.buf.rename, bufnr)
+  lsp_map("<leader>rn", vim.lsp.buf.rename, bufnr)
 
   -- Code actions
-	lsp_map("<leader>ca", vim.lsp.buf.code_action, bufnr)
+  lsp_map("<leader>ca", vim.lsp.buf.code_action, bufnr)
 
   -- Goto definition
-	lsp_map("gd", builtins.lsp_definitions, bufnr)
+  lsp_map("gd", builtins.lsp_definitions, bufnr)
 
   -- Goto references
-	lsp_map("gr", builtins.lsp_references, bufnr)
+  lsp_map("gr", builtins.lsp_references, bufnr)
 
   -- Show hover documentation
-	lsp_map("K", vim.lsp.buf.hover, bufnr)
+  lsp_map("K", vim.lsp.buf.hover, bufnr)
 end
 
 return on_attach
