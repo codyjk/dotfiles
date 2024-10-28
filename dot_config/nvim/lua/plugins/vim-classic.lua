da6873d679ebc692225e130565d8d7bc6434f05f
Cody Joseph Krainock <me@codyjoseph.com>
2024-10-27 17:10:54 -0700

    Improve pairs/surround

--- dot_config/nvim/lua/plugins/vim-classic.lua
+++ dot_config/nvim/lua/plugins/vim-classic.lua
@@ -1,10 +1,8 @@
 return {
   "tpope/vim-rhubarb",
-  "tpope/vim-surround",
   "rust-lang/rust.vim",
   "christoomey/vim-tmux-navigator",
   "tpope/vim-vinegar",
-  "windwp/nvim-autopairs",
   {
     "numToStr/Comment.nvim",
     lazy = false,
